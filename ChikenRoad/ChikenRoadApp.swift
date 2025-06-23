595acea55d3b160b10ebf66d75d03fdfaca924c0
keshew <artyom.korotckov2017@yandex.ru>
2025-06-23 16:25:00 +0500

    Initial Commit

---
+++ ChikenRoad/ChikenRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChikenRoadApp.swift
+//  ChikenRoad
+//
+//  Created by Артём Коротков on 23.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChikenRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
